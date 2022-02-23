71656220b421e44cf9e59f06fda083b5af5d3919
monkey <monkey007008@gmail.com>
2022-02-23 18:44:58 +0800

    m

--- js/i18n.js
+++ js/i18n.js
@@ -1,28 +1,49 @@
 $(document).ready(function () {
-    /*默认语言*/
-    var defaultLang = "en";
+  /*默认语言*/
+  var defaultLang = "en";
+  $("[i18n]").i18n({
+    defaultLang: defaultLang,
+    filePath: "/i18n/",
+    filePrefix: "i18n_",
+    fileSuffix: "",
+    forever: true,
+    callback: function () {
+      console.log("i18n is ready.");
+    },
+  });
+  /*切换为中文 - 按钮*/
+  $(".chinese").click(function () {
     $("[i18n]").i18n({
-      defaultLang: defaultLang,
+      defaultLang: "cn",
       filePath: "/i18n/",
-      filePrefix: "i18n_",
-      fileSuffix: "",
-      forever: true,
-      callback: function () {
-        console.log("i18n is ready.");
-      },
-    });
-    /*切换为中文 - 按钮*/
-    $(".chinese").click(function () {
-      $("[i18n]").i18n({
-        defaultLang: "cn",
-        filePath: "/i18n/",
-      });
-    });
-    /*切换为英文 - 按钮*/
-    $(".english").click(function () {
-      $("[i18n]").i18n({
-        defaultLang: "en",
-        filePath: "/i18n/",
-      });
     });
   });
+  /*切换为英文 - 按钮*/
+  $(".english").click(function () {
+    $("[i18n]").i18n({
+      defaultLang: "en",
+      filePath: "/i18n/",
+    });
+  });
+  /*切换为繁体 - 按钮*/
+  $(".traditional").click(function () {
+    $("[i18n]").i18n({
+      defaultLang: "fan",
+      filePath: "/i18n/",
+    });
+  });
+  /*切换为法文 - 按钮*/
+  $(".french").click(function () {
+    $("[i18n]").i18n({
+      defaultLang: "french",
+      filePath: "/i18n/",
+    });
+  });
+  /*切换为日文 - 按钮*/
+  $(".japanese").click(function () {
+    $("[i18n]").i18n({
+      defaultLang: "jpan",
+      filePath: "/i18n/",
+    });
+  });
+});
